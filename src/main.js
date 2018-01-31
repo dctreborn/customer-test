219d41ff7cee6e0edb37088c7a2b5ce6e80f6ce9
rukaroa <rukaroa@gmail.com>
2018-01-31 10:19:18 -0600

    navbar added with appropriate routing

--- src/main.js
+++ src/main.js
@@ -32,7 +32,26 @@ new Vue({
   router,
   template: `
     <div id='app'>
-      <router-link to="/about">About</router-link>
+      <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">vCustomers</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/about">About</router-link></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+    
       <router-view></router-view>
     </div>
   `
