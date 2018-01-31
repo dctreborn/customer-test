e9fb0bc09d107a3cdb20c514b369a88bed5357c8
rukaroa <rukaroa@gmail.com>
2018-01-31 09:47:19 -0600

    basic router implemented

--- src/main.js
+++ src/main.js
@@ -2,12 +2,38 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+import vueResource from 'vue-resource'
+import Customers from './components/Customers.vue'
+import About from './components/About.vue'
+
+Vue.use(vueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {
+      path: '/',
+      component: Customers
+    },
+    {
+      path: '/about',
+      component: About
+    }
+  ]
+})
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+  router,
+  template: `
+    <div id='app'>
+      <router-link to="/about">About</router-link>
+      <router-view></router-view>
+    </div>
+  `
+}).$mount('#app')
