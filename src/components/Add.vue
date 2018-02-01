79de8885f91fd04edf35f090c1ce5064327a22ce
rukaroa <rukaroa@gmail.com>
2018-02-01 11:46:11 -0600

    basic form added

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,6 +1,48 @@
 <template>
   <div class="about container">
       <h1 class="page-header">Add Customer</h1>
+      <form v-on:submit="addCustomer">
+          <div class="well">
+            <h4>Customer Info</h4>
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" class="form-control" placeholder="Name" v-model="customer.name">
+            </div>
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" class="form-control" placeholder="Username" v-model="customer.username">
+            </div>
+          </div>
+
+          <div class="well">
+            <h4>Customer Contact</h4>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="text" class="form-control" placeholder="Email" v-model="customer.email">
+            </div>
+            <div class="form-group">
+                <label>Phone</label>
+                <input type="text" class="form-control" placeholder="Phone" v-model="customer.phone">
+            </div>
+          </div>
+
+          <div class="well">
+            <h4>Customer Location</h4>
+            <div class="form-group">
+                <label>Address</label>
+                <input type="text" class="form-control" placeholder="Address" v-model="customer.address">
+            </div>
+            <div class="form-group">
+                <label>City</label>
+                <input type="text" class="form-control" placeholder="City" v-model="customer.city">
+            </div>
+            <div class="form-group">
+                <label>State</label>
+                <input type="text" class="form-control" placeholder="State" v-model="customer.state">
+            </div>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
   </div>
 </template>
 
@@ -9,7 +51,7 @@ export default {
   name: 'add',
   data () {
     return {
-     
+        customer: {}
     }
   }
 }
